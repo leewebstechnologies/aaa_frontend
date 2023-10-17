49d99f46328689c0b86da26cb109a346b5a5831d
leewebstechnologies <aliuajibade317@gmail.com>
2023-10-17 14:30:31 +0100

    Appwrap

---
+++ src/components/NavigationDots.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable jsx-a11y/anchor-has-content */
+
+const NavigationDots = ({ active }) => {
+  return (
+    <div className="app__navigation">
+      {["home", "about", "work", "skills", "testimonials", "contact"].map(
+        (item, index) => (
+          <a
+            href={`#${item}`}
+            key={item + index}
+            className="app__navigation-dot"
+            style={active === item ? { backgroundColor: "#313bac" } : {}}
+            alt="dots"
+          />
+        )
+      )}
+    </div>
+  );
+};
+
+export default NavigationDots;
